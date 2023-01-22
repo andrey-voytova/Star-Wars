740a915af35665e0fca86c1f9bd37d2ed96e7a69
andrey-voytova <andrey.voytovaa@yandex.ru>
2023-01-22 13:49:43 +0300

    deploy

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import App from '@containers/App';
 
 import './index.css';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import store from './store/store'
 import ThemeProvider from './context/ThemeProvider';
@@ -12,13 +12,13 @@ import ThemeProvider from './context/ThemeProvider';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
 
-    <BrowserRouter  >
+    <HashRouter  >
         <Provider store={store}>
             <ThemeProvider>
                 <App/>
             </ThemeProvider>
         </Provider>
-    </BrowserRouter>
+    </HashRouter>
     
 );
 
