c6881cc517490b3f9c4b7ec035a937054021d616
andrey-voytova <andrey.voytovaa@yandex.ru>
2023-01-22 13:29:33 +0300

    add

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import store from './store/store'
 import ThemeProvider from './context/ThemeProvider';
-import { REPO_NAME } from './constants/repo';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
